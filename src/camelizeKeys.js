59ac7208e63d4084c9943ed998e2dd19550f3821
gin0606 <kkgn06@gmail.com>
2016-11-11 16:22:13 +0900

    Remove camelizejs

--- src/camelizeKeys.js
+++ src/camelizeKeys.js
@@ -1,4 +1,5 @@
-import camelize from 'camelizejs'
+import camelCase from 'lodash.camelcase'
+import upperFirst from 'lodash.upperfirst'
 
 function isObject (o) {
   return typeof o === 'object' && o !== null && !Array.isArray(o)
@@ -6,9 +7,10 @@ function isObject (o) {
 
 export default function camelizeKeys (o, uppercaseFirstLetter = true) {
   if (!isObject(o)) { return o }
+  const f = uppercaseFirstLetter ? (s) => (upperFirst(camelCase(s))) : camelCase
   return Object.keys(o).reduce((result, key) => {
     let a = {}
-    a[camelize(key, uppercaseFirstLetter)] = camelizeKeys(o[key], uppercaseFirstLetter)
+    a[f(key)] = camelizeKeys(o[key], uppercaseFirstLetter)
     return Object.assign(result, a)
   }, {})
 }
