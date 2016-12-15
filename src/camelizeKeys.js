398a2da047bbbd9fcd850246667a324ab6ef0b9d
gin0606 <kkgn06@gmail.com>
2016-12-15 16:13:50 +0900

    Split method uppercaseFirstLetter condition

--- src/camelizeKeys.js
+++ src/camelizeKeys.js
@@ -5,12 +5,20 @@ function isObject (o) {
   return typeof o === 'object' && o !== null && !Array.isArray(o)
 }
 
-export default function camelizeKeys (o, uppercaseFirstLetter = true) {
+function camelize (o, uppercaseFirstLetter) {
   if (!isObject(o)) { return o }
   const f = uppercaseFirstLetter ? (s) => (upperFirst(camelCase(s))) : camelCase
   return Object.keys(o).reduce((result, key) => {
     let a = {}
-    a[f(key)] = camelizeKeys(o[key], uppercaseFirstLetter)
+    a[f(key)] = camelize(o[key], uppercaseFirstLetter)
     return Object.assign(result, a)
   }, {})
+}
+
+export function CamelizeKeys (o) {
+  return camelize(o, true)
+}
+
+export function camelizeKeys (o) {
+  return camelize(o, false)
 }
--- test/camelizeKeys.spec.js
+++ test/camelizeKeys.spec.js
@@ -1,19 +1,19 @@
 import assert from 'power-assert'
-import camelizeKeys from '../src/camelizeKeys'
+import { CamelizeKeys, camelizeKeys } from '../src/camelizeKeys'
 
-describe('#camelizeKeys', () => {
-  context('with object', () => {
-    const o = {
-      hoge1: true,
-      hoge2: false,
-      hoge3: {
-        fuga1: 'fuga_value',
-        fuga2: {
-          hoge_fuga: 100
-        }
+describe('#camelize-object-keys', () => {
+  const o = {
+    hoge1: true,
+    hoge2: false,
+    hoge3: {
+      fuga1: 'fuga_value',
+      fuga2: {
+        hoge_fuga: 100
       }
     }
-    context('when `uppercaseFirstLetter` is true', () => {
+  }
+  describe('#CamelizeKeys', () => {
+    context('with object', () => {
       const expected = {
         Hoge1: true,
         Hoge2: false,
@@ -25,12 +25,26 @@ describe('#camelizeKeys', () => {
         }
       }
       it('returns UpperCamelCase key object ', () => {
-        let camelizedObject = camelizeKeys(o)
+        let camelizedObject = CamelizeKeys(o)
         assert.deepEqual(camelizedObject, expected)
       })
     })
 
-    context('when `uppercaseFirstLetter` is false', () => {
+    context('with string', () => {
+      it('does not change string', () => {
+        assert.equal(CamelizeKeys('string'), 'string')
+      })
+    })
+
+    context('with array', () => {
+      it('does not change array', () => {
+        assert.deepEqual(CamelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga'])
+      })
+    })
+  })
+
+  describe('#camelizeKeys', () => {
+    context('with object', () => {
       const expected = {
         hoge1: true,
         hoge2: false,
@@ -46,17 +60,17 @@ describe('#camelizeKeys', () => {
         assert.deepEqual(camelizedObject, expected)
       })
     })
-  })
 
-  context('with string', () => {
-    it('does not change string', () => {
-      assert.equal(camelizeKeys('string'), 'string')
+    context('with string', () => {
+      it('does not change string', () => {
+        assert.equal(camelizeKeys('string'), 'string')
+      })
     })
-  })
 
-  context('with array', () => {
-    it('does not change array', () => {
-      assert.deepEqual(camelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga'])
+    context('with array', () => {
+      it('does not change array', () => {
+        assert.deepEqual(camelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga'])
+      })
     })
   })
 })
