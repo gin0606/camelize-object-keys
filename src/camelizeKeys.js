d2566dadfd10bcac0eb4c82dc92abdae26df9495
gin0606 <kkgn06@gmail.com>
2016-12-15 16:29:31 +0900

    Change ESLint rule to airbnb

--- src/camelizeKeys.js
+++ src/camelizeKeys.js
@@ -1,24 +1,24 @@
-import camelCase from 'lodash.camelcase'
-import upperFirst from 'lodash.upperfirst'
+import camelCase from 'lodash.camelcase';
+import upperFirst from 'lodash.upperfirst';
 
-function isObject (o) {
-  return typeof o === 'object' && o !== null && !Array.isArray(o)
+function isObject(o) {
+  return typeof o === 'object' && o !== null && !Array.isArray(o);
 }
 
-function camelize (o, uppercaseFirstLetter) {
-  if (!isObject(o)) { return o }
-  const f = uppercaseFirstLetter ? (s) => (upperFirst(camelCase(s))) : camelCase
+function camelize(o, uppercaseFirstLetter) {
+  if (!isObject(o)) { return o; }
+  const f = uppercaseFirstLetter ? s => (upperFirst(camelCase(s))) : camelCase;
   return Object.keys(o).reduce((result, key) => {
-    let a = {}
-    a[f(key)] = camelize(o[key], uppercaseFirstLetter)
-    return Object.assign(result, a)
-  }, {})
+    const a = {};
+    a[f(key)] = camelize(o[key], uppercaseFirstLetter);
+    return Object.assign(result, a);
+  }, {});
 }
 
-export function CamelizeKeys (o) {
-  return camelize(o, true)
+export function CamelizeKeys(o) {
+  return camelize(o, true);
 }
 
-export function camelizeKeys (o) {
-  return camelize(o, false)
+export function camelizeKeys(o) {
+  return camelize(o, false);
 }
--- test/camelizeKeys.spec.js
+++ test/camelizeKeys.spec.js
@@ -1,5 +1,5 @@
-import assert from 'power-assert'
-import { CamelizeKeys, camelizeKeys } from '../src/camelizeKeys'
+import assert from 'power-assert';
+import { CamelizeKeys, camelizeKeys } from '../src/camelizeKeys';
 
 describe('#camelize-object-keys', () => {
   const o = {
@@ -8,10 +8,10 @@ describe('#camelize-object-keys', () => {
     hoge3: {
       fuga1: 'fuga_value',
       fuga2: {
-        hoge_fuga: 100
-      }
-    }
-  }
+        hoge_fuga: 100,
+      },
+    },
+  };
   describe('#CamelizeKeys', () => {
     context('with object', () => {
       const expected = {
@@ -20,28 +20,28 @@ describe('#camelize-object-keys', () => {
         Hoge3: {
           Fuga1: 'fuga_value',
           Fuga2: {
-            HogeFuga: 100
-          }
-        }
-      }
+            HogeFuga: 100,
+          },
+        },
+      };
       it('returns UpperCamelCase key object ', () => {
-        let camelizedObject = CamelizeKeys(o)
-        assert.deepEqual(camelizedObject, expected)
-      })
-    })
+        const camelizedObject = CamelizeKeys(o);
+        assert.deepEqual(camelizedObject, expected);
+      });
+    });
 
     context('with string', () => {
       it('does not change string', () => {
-        assert.equal(CamelizeKeys('string'), 'string')
-      })
-    })
+        assert.equal(CamelizeKeys('string'), 'string');
+      });
+    });
 
     context('with array', () => {
       it('does not change array', () => {
-        assert.deepEqual(CamelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga'])
-      })
-    })
-  })
+        assert.deepEqual(CamelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga']);
+      });
+    });
+  });
 
   describe('#camelizeKeys', () => {
     context('with object', () => {
@@ -51,26 +51,26 @@ describe('#camelize-object-keys', () => {
         hoge3: {
           fuga1: 'fuga_value',
           fuga2: {
-            hogeFuga: 100
-          }
-        }
-      }
+            hogeFuga: 100,
+          },
+        },
+      };
       it('returns lowerCamelCase key object ', () => {
-        let camelizedObject = camelizeKeys(o, false)
-        assert.deepEqual(camelizedObject, expected)
-      })
-    })
+        const camelizedObject = camelizeKeys(o, false);
+        assert.deepEqual(camelizedObject, expected);
+      });
+    });
 
     context('with string', () => {
       it('does not change string', () => {
-        assert.equal(camelizeKeys('string'), 'string')
-      })
-    })
+        assert.equal(camelizeKeys('string'), 'string');
+      });
+    });
 
     context('with array', () => {
       it('does not change array', () => {
-        assert.deepEqual(camelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga'])
-      })
-    })
-  })
-})
+        assert.deepEqual(camelizeKeys(['hoge', 'fuga']), ['hoge', 'fuga']);
+      });
+    });
+  });
+});
